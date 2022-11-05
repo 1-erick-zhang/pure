b1fed532ac1e520e0359f99701114086c5ae9e3e
Erick Zhang <62749050+1-erick-zhang@users.noreply.github.com>
2022-11-05 07:38:05 -0300

    Update _pure_prompt_ssh.fish

--- functions/_pure_prompt_ssh.fish
+++ functions/_pure_prompt_ssh.fish
@@ -1,5 +1,8 @@
 function _pure_prompt_ssh
     if test "$SSH_CONNECTION" != ""
-        echo (_pure_user_at_host)
+        set --local ssh_string = "SSH";
+        set --local ssh_color (_pure_set_color $pure_color_ssh)
+        
+        echo "$ssh_color$ssh_string"
     end
 end
